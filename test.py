9b6e086e6eb1bf279c801192ccc5ce3188abbd31
DanielZhuGY <91763116+DanielZhuGY@users.noreply.github.com>
2021-10-04 23:25:25 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
-
 import chess
-
+turn = 0
 board = chess.Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
 print(board)
 while board.legal_moves != 0:
-    #a = board.piece_map()   #where the piece is<a dictionary>
+    turn = turn +1
+    print('{} round'.format(turn))
+    b = board.piece_map()   #where the piece is<a dictionary>
+    if turn%2 != 0:
+        print('white turn')
+    else:
+        print('black turn')
     a = board.legal_moves
 
     print(a)
@@ -12,3 +17,4 @@ while board.legal_moves != 0:
     move = chess.Move.from_uci(str(board.parse_san(i)))
     board.push(move)
     print(board)
+
