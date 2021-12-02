75199903019b54e7c7590c874cb693cb905a0f39
DanielZhuGY <91763116+DanielZhuGY@users.noreply.github.com>
2021-12-01 23:29:56 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,21 @@
 import chess
+import chess.polyglot
+
+def eva(board):
+    with chess.polyglot.open_reader("Human.bin") as reader:
+        eva_dic = {}#create a dictionary which key is the move and value is the weight
+        for entry in reader.find_all(board,minimum_weight = 0):
+            eva_dic[entry.move] = entry.weight
+        return(eva_dic)
+
 def move():
     turn = 0
     board = chess.Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')#initialize the board
     print(board)
+ 
     while board.legal_moves != 0:# the game will end if there's no legal move for one player
+        print(eva(board))
+
         turn = turn +1
         print('{} round'.format(turn))
         #b = board.piece_map()   #where the piece is<a dictionary>
@@ -18,7 +30,7 @@ def move():
         move = chess.Move.from_uci(str(board.parse_san(i)))
         board.push(move)
         print(board)
-    return(i)
+    return(turn)
 
 
 def main():
@@ -33,4 +45,6 @@ def main():
         new = input('type 1 if you want to start a new game')
         if new == 1:
             n = 1
+
+
 main()
